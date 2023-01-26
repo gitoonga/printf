6057b47bb59a1cd48f253292e68d9952167a19d8
Clinohub <esmicorreo09@gmail.com>
2023-01-27 02:09:22 +0300

    Betty done

--- parser.c
+++ parser.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * parser - Receives the main string and all the necessary parameters to
  * print a formatted string.
@@ -10,46 +9,44 @@
  */
 int parser(const char *main_string, conver_t function_list[], va_list arg_list)
 {
-    int i, j, return_val, printed_chars;
+	int i, j, _return_val, printed_chars;
 
-    printed_chars = 0;
+	printed_chars = 0;/* Iterates through the main string */
+	for (i = 0; main_string[i] != '\0'; i++)
+	{/* Check if current character is a format specifier */
+		if (main_string[i] == '%')
+		{/* Iterate through the list of possible functions to find the correct one */
+			for (j = 0; function_list[j].symbol != NULL; j++)
+			{/* If a matching function is found, call it and...*/
+				/*...increment the printed character count */
+				if (main_string[i + 1] == function_list[j].symbol[0])
+				{
+					_return_val = (function_list[j].func(arg_list));
+					if (_return_val == (-1))
+						return (-1);
 
-    /* Iterates through the main string */
-    for (i = 0; main_string[i] != '\0'; i++) {
-        
-        /* Check if current character is a format specifier */
-        if (main_string[i] == '%') {
-            
-            /* Iterate through the list of possible functions to find the correct one */
-            for (j = 0; function_list[j].symbol != NULL; j++) {
-                
-                /* If a matching function is found, call it and increment the printed character count */
-                if (main_string[i + 1] == function_list[j].symbol[0]) {
-                    return_val = function_list[j].func(arg_list);
-                    if (return_val == -1)
-                        return (-1);
-                    printed_chars += return_val;
-                    break;
-                }
-            }
-            
-            /* If no matching function is found */
-            if (function_list[j].symbol == NULL && main_string[i + 1] != ' ') {
-                if (main_string[i + 1] != '\0') {
-                    _writechar(main_string[i]);
-                    _writechar(main_string[i + 1]);
-                    printed_chars = printed_chars + 2;
-                } else {
-                    return (-1);
-                }
-            }
-            
-            /* Update loop variable to skip format symbolbols */
-            i = i + 1; 
-        } else {
-            _writechar(main_string[i]); /* call the write function */
-            printed_chars++;
-        }
-    }
-    return (printed_chars);
+					printed_chars += _return_val;
+					break;
+				}
+			} /* If no matching function is found */
+			if (function_list[j].symbol == NULL && main_string[i + 1] != ' ')
+			{
+				if (main_string[i + 1] != '\0')
+				{
+					_writechar(main_string[i]);
+					_writechar(main_string[i + 1]);
+					printed_chars = printed_chars + 2;
+				}
+				else
+					return (-1);
+			} /* Update loop variable to skip format symbolbols */
+			i = i + 1;
+		}
+		else
+		{
+			_writechar(main_string[i]); /* call the write function */
+			printed_chars++;
+		}
+	}
+	return (printed_chars);
 }
